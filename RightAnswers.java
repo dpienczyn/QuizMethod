ca402be40ed5a536d6803497f1e67a9a63826f10
dpienczyn <donia19881@wp.pl>
2019-08-15 18:19:06 +0200

    Update RightAnswers.java

--- RightAnswers.java
+++ RightAnswers.java
@@ -2,29 +2,29 @@ package Quiz;
 
 public class RightAnswers {
 
-	public String equivalent;
+    public String equivalent;
 
-	public String rightAnswer;
+    public String rightAnswer;
 
-	public RightAnswers(String equivalent, String rightAnswer) {
-		super();
-		this.equivalent = equivalent;
-		this.rightAnswer = rightAnswer;
-	}
+    public RightAnswers(String equivalent, String rightAnswer) {
+        super();
+        this.equivalent = equivalent;
+        this.rightAnswer = rightAnswer;
+    }
 
-	public String getEquivalent() {
-		return equivalent;
-	}
+    public String getEquivalent() {
+        return equivalent;
+    }
 
-	public void setEquivalent(String equivalent) {
-		this.equivalent = equivalent;
-	}
+    public void setEquivalent(String equivalent) {
+        this.equivalent = equivalent;
+    }
 
-	public String getRightAnswer() {
-		return rightAnswer;
-	}
+    public String getRightAnswer() {
+        return rightAnswer;
+    }
 
-	public void setRightAnswer(String rightAnswer) {
-		this.rightAnswer = rightAnswer;
-	}
+    public void setRightAnswer(String rightAnswer) {
+        this.rightAnswer = rightAnswer;
+    }
 }
