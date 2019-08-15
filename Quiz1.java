57dfb05f4b72a3df2c2fdabd34077a96d1cb7276
dpienczyn <donia19881@wp.pl>
2019-08-15 18:19:48 +0200

    Update Quiz1.java

--- Quiz1.java
+++ Quiz1.java
@@ -4,50 +4,50 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Quiz1{
-	
-	public void start() {
-		System.out.println("**********Quiz Przyrodniczy**********");
-		System.out.println("Na ekranie bed¹ wyœwietlaæ siê pytania na które trzeba odpowiadaæ: a, b, c lub d.");
-		System.out.println("Odpowiedz jest tylko jedna!");
-		System.out.println("Do dzie³a -->");
-		System.out.println();
-	}
+public class Quiz1 {
 
-	public String userAnswer() {
-		Scanner in = new Scanner(System.in);
-		String r;
-		r = in.nextLine();
-		return r;
-	}
-	
-	int score = 0;
-	int i=0;
-	
-	public int addScore( String userAnswer, List<RightAnswers> rightAnswers) {
-		
-		if(userAnswer.equals(rightAnswers.get(i).getEquivalent())) {
-			System.out.println("Odpowiedz jest prawid³owa! Brawo!");
-			score++;
-		}else {
-			System.out.println("Twoja odpowiedz nie jest dobra");
-			System.out.println();
-		}
-		i++;
-		return score;
-	}
-	
-	public void score(int score) {
-		char newLine = '\n';
-		System.out.println(newLine + "Twoja liczba punktów: " + score);
-		System.out.println();
-	}
-	
-	
-	public static void main(String[] args) {
-		Quiz1 quiz = new Quiz1();
-		DataAllList data = new DataAllList();
-		quiz.start();
+    public void start() {
+        System.out.println("**********Quiz Przyrodniczy**********");
+        System.out.println("Na ekranie bedÂ¹ wyÅ“wietlaÃ¦ siÃª pytania na ktÃ³re trzeba odpowiadaÃ¦: a, b, c lub d.");
+        System.out.println("Odpowiedz jest tylko jedna!");
+        System.out.println("Do dzieÂ³a -->");
+        System.out.println();
+    }
+
+    public String userAnswer() {
+        Scanner in = new Scanner(System.in);
+        String r;
+        r = in .nextLine();
+        return r;
+    }
+
+    int score = 0;
+    int i = 0;
+
+    public int addScore(String userAnswer, List < RightAnswers > rightAnswers) {
+
+        if (userAnswer.equals(rightAnswers.get(i).getEquivalent())) {
+            System.out.println("Odpowiedz jest prawidÂ³owa! Brawo!");
+            score++;
+        } else {
+            System.out.println("Twoja odpowiedz nie jest dobra");
+            System.out.println();
+        }
+        i++;
+        return score;
+    }
+
+    public void score(int score) {
+        char newLine = '\n';
+        System.out.println(newLine + "Twoja liczba punktÃ³w: " + score);
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+            Quiz1 quiz = new Quiz1();
+            DataAllList data = new DataAllList();
+            quiz.start();
 		List<Questions> questions = data.DownloadQuestion();
 		List<Answers> answers = data.DownloadAnswer();
 		List<RightAnswers> rightAnswers = data.DownloadRightAnswer();
